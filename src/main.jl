115b4ca4ee4715cdc79f47d5b8e14c82bfc8367c
JAL <tsukisan@gmail.com>
2026-02-12 14:50:47 +0200

    Visualizing...

--- src/main.jl
+++ src/main.jl
@@ -1,15 +1,34 @@
 using RigidBodyDynamics
+using MeshCat
+using MeshCatMechanisms
 
 function main()
-    # Cargar modelo
+    # 1. Cargar modelo
     urdf_path = joinpath(@__DIR__, "..", "models", "furuta.urdf")
     mechanism = parse_urdf(urdf_path)
     state = MechanismState(mechanism)
     
-    println("✅ Modelo cargado correctamente con RigidBodyDynamics.jl")
-    println("Grados de libertad: ", num_velocities(mechanism))
+    println(urdf_path)
+    println("✅ Modelo cargado correctamente")
+
+    # 2. Configurar el Visualizador
+    #vis = Visualizer() # Esto crea el servidor de MeshCat
+    #mcv = MechanismVisualizer(mechanism, URDFVisuals(urdf_path), vis)
+
+    mvis = MechanismVisualizer(mechanism, URDFVisuals(urdf_path));
+    
+
+
+    # 3. Ejemplo: Mover el péndulo a una posición específica
+    # Supongamos que q[1] es el brazo motor y q[2] es el péndulo
+    set_configuration!(mcv, [0.0, 3.14]) # Poner el péndulo hacia arriba (aprox π)
+
+    println("🚀 Visualización abierta en el navegador. Revisa la URL (usualmente localhost:8080)")
+    
+    return mvis
 end
 
+mvis = main()
 
-main()
+render(vis) # Emsto abre o actualiza la pestaña
 
